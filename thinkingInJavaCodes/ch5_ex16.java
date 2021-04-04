2adde39518c6f75e6744fe255bb9f2e4c2219f95
vzuer <vzuers@163.com>
2021-04-04 11:32:45 +0800

    simple example for test

---
+++ thinkingInJavaCodes/ch5_ex16.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ch5_ex16{
+	public static void main(String[] args){
+		String[] str = new String[]{"str_test", "data_str", "a_simple_string"};
+		for(int i=0; i<str.length; ++i){
+			System.out.println(str[i]);
+		}
+		System.out.println(Arrays.toString(str));
+	}
+}
