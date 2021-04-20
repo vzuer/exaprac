05b0461bb1f2bc84cd21fd4dddac13eac6c76558
vzuer <vzuers@163.com>
2021-04-20 18:54:02 +0800

    update file Day05.java

--- shkJavaSE/DailyPractise/Day05.java
+++ shkJavaSE/DailyPractise/Day05.java
@@ -6,7 +6,21 @@ public class Day05{
         int a = scan.nextInt();
         System.out.println("请输入第二个正整数：");
         int b = scan.nextInt();
-        // iteration
-        int min = 
+        // iteration method: to get the gcd of a and b
+        int min = (a > b) ? b : a;
+        for(int i=min; i>0; --i){
+            if(a%i == 0 && b%i == 0){
+                System.out.println("这两个数的最大公约数是：" + i);
+                break;
+            }
+        }
+        // get the lcm of a and b
+        int max = (a > b) ? a : b;
+        for(int i=max; i<=a*b; ++i){
+            if(i%a==0 && i%b==0){
+                System.out.println("这两个数的最小公倍数是：" + i);
+                break;
+            }
+        }
     }
 }
